53526b0655a240949905587c4a334c1bc40f108b
Uraswa <33593469+Uraswa@users.noreply.github.com>
2020-03-25 18:56:50 +0300

    refactor(Menu)

--- catalog/controller/common/menu.php
+++ catalog/controller/common/menu.php
@@ -4,14 +4,21 @@ class ControllerCommonMenu extends Controller {
 
     public function index(){
         $this->load->language('common/menu');
+        $this->load->model('catalog/category');
 
-        $data['items'] = [
-            'about',
-            'apparel',
-            'surf_apparel',
-            'windsurf',
-            'kitesurf',
-            'accessories',
+        $categories = $this->model_catalog_category->getCategories();
+
+        $items = [[$this->language->get('about'), '']];
+
+        foreach ($categories as $category) {
+            $items[] = [
+                $category['name'],
+                $this->url->link('product/category', 'path='.$category['category_id']),
+                isset($this->request->get['path']) && $this->request->get['path'] == $category['category_id'] && $this->request->get['route'] == 'product/category'
+            ];
+        }
+
+        $other_links = [
             'sale',
             'brands',
             'blog',
@@ -19,11 +26,12 @@ class ControllerCommonMenu extends Controller {
             'contact'
         ];
 
-        foreach ($data['items'] as $item){
-            $data[$item] = $this->language->get($item);
+        foreach ($other_links as $other_link){
+            $items[] = [$this->language->get($other_link), '', false];
         }
 
         $data['menu'] = $this->language->get('menu');
+        $data['items'] = $items;
 
 
 
--- catalog/view/theme/default/template/common/menu.tpl
+++ catalog/view/theme/default/template/common/menu.tpl
@@ -3,9 +3,7 @@
     <li class="menu__title raleway bg-dark">
         <?php echo $menu ?>
     </li>
-    <?php
-        foreach($items as $item) {
-            echo "<li class='menu__item raleway text-dark'><a href=''>${$item}</a></li>";
-        }
-    ?>
+    <?php foreach($items as $item) { ?>
+            <li class='menu__item <?php echo $item[2] ? "selected" : "" ?> raleway text-dark'><a href='<?php echo $item[1] ?>'><?php echo $item[0] ?></a></li>
+    <?php }?>
 </ul>
