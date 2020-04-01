f0e63b6abb2787e7b618fb4272297db2018a5e4e
Uraswa <33593469+Uraswa@users.noreply.github.com>
2020-04-01 18:23:34 +0300

    refactor(ProductItem) link construction includes path and manufacturer id

--- catalog/controller/product/product_item.php
+++ catalog/controller/product/product_item.php
@@ -4,7 +4,9 @@ class ControllerProductProductItem extends Controller {
 
     public function index($product){
         $product['image'] = 'image/' . $product['image'];
-        $product['link'] = $this->url->link('product/product', 'product_id=' . $product['product_id']);
+        $path = isset($this->request->get['path']) ? "&path=".$this->request->get['path'] : '';
+        $manufacturer = isset($this->request->get['manufacturer']) ? "&manufacturer=".$this->request->get['manufacturer'] : '';
+        $product['link'] = $this->url->link('product/product', 'product_id=' . $product['product_id'] . $path . $manufacturer);
         $product['price'] = $this->currency->format($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
         if (isset($product['special']) && $product['special'] != null) {
             $product['special'] = $this->currency->format($this->tax->calculate($product['special'], $product['special'], $this->config->get('config_tax')), $this->session->data['currency']);
