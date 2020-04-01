916649cd2cc03af5512ba62817b80092461d4a49
Uraswa <33593469+Uraswa@users.noreply.github.com>
2020-04-01 18:21:18 +0300

    fix(CategoryOptions) null route index error

--- catalog/controller/product/category_options.php
+++ catalog/controller/product/category_options.php
@@ -5,16 +5,20 @@ class ControllerProductCategoryOptions extends Controller {
     public function index() {
         $this->load->language('product/category_options');
         $this->load->model('catalog/category');
+        $items = [];
 
         $data['category_options_text'] = $this->language->get('category_options');
-        $main_category = explode('_', $this->request->get['path'])[0];
-        $categories = $this->model_catalog_category->getCategories($main_category);
-        $items = [];
-        $manufacturer = isset($this->request->get['manufacturer']) ? "manufacturer=".$this->request->get['manufacturer'] : '';
-        foreach ($categories as $category) {
-            $path = $main_category.'_'.$category['category_id'];
-            $url = "$manufacturer&path=".$path;
-            $items[] = [$category['name'], $this->url->link('product/category', $url) , $this->request->get['path'] == $path];
+        if (isset($this->request->get['path'])) {
+            $main_category = explode('_', $this->request->get['path'])[0];
+            $categories = $this->model_catalog_category->getCategories($main_category);
+
+            $manufacturer = isset($this->request->get['manufacturer']) ? "manufacturer=" . $this->request->get['manufacturer'] : '';
+            foreach ($categories as $category) {
+                $path = $main_category . '_' . $category['category_id'];
+                $url = "$manufacturer&path=" . $path;
+                $items[] = [$category['name'], $this->url->link('product/category', $url), $this->request->get['path'] == $path];
+            }
+
         }
 
         return $this->load->view('common/left_menu', [
