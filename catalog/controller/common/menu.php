4ccefcdaa1422ae5fe02ffeac33830f3bada1705
Uraswa <33593469+Uraswa@users.noreply.github.com>
2020-04-15 15:59:42 +0300

    refactor(Menu) search page adaptation

--- catalog/controller/common/menu.php
+++ catalog/controller/common/menu.php
@@ -6,14 +6,27 @@ class ControllerCommonMenu extends Controller {
         $this->load->language('common/menu');
         $this->load->model('catalog/category');
 
+        $search = isset($this->request->get['search']) ? '&search='. $this->request->get['search'] : '';
+        $sub_category = isset($this->request->get['sub_category']) ? '&sub_category='. $this->request->get['sub_category'] : '';
+        $description = isset($this->request->get['description']) ? '&description='. $this->request->get['description'] : '';
+        $link_template = $this->url->link('%s', '%s' . $search . $sub_category . $description);
+
+        if (isset($this->request->get['route']) && $this->request->get['route'] == 'product/search'){
+            $link_template = sprintf($link_template,'product/search', 'category_id=%s');
+            $current_category = isset($this->request->get['category_id']) ? $this->request->get['category_id'] : 0;
+            $main_category = end($this->model_catalog_category->getParentCategories($current_category));
+        } else {
+            $link_template = sprintf($link_template,'product/category', 'path=%s');
+            $main_category = explode('_', isset($this->request->get['path']) ? $this->request->get['path'] : '')[0];
+        }
+
         $categories = $this->model_catalog_category->getCategories();
 
         $items = [[$this->language->get('about'), '']];
-        $main_category = explode('_', isset($this->request->get['path']) ? $this->request->get['path'] : '')[0];
         foreach ($categories as $category) {
             $items[] = [
                 $category['name'],
-                $this->url->link('product/category', 'path='.$category['category_id']),
+                sprintf($link_template, $category['category_id']),
                 $main_category == $category['category_id']
             ];
         }
