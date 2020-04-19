df33cd59774b904a4d2122054635d72a33230665
Uraswa <33593469+Uraswa@users.noreply.github.com>
2020-04-19 12:30:04 +0300

    fix(ProductPage) manufacturer id

--- catalog/controller/product/product_item.php
+++ catalog/controller/product/product_item.php
@@ -3,9 +3,9 @@
 class ControllerProductProductItem extends Controller {
 
     public function index($product){
-        $product['image'] = 'image/' . $product['image'];
+       $product['image'] = 'image/' . $product['image'];
         $path = isset($this->request->get['path']) ? "&path=".$this->request->get['path'] : '';
-        $manufacturer = isset($this->request->get['manufacturer']) ? "&manufacturer=".$this->request->get['manufacturer'] : '';
+        $manufacturer = isset($this->request->get['manufacturer']) ? "&manufacturer=".$this->request->get['manufacturer'] : "&manufacturer=".$product['manufacturer_id'];
         $product['link'] = $this->url->link('product/product', 'product_id=' . $product['product_id'] . $path . $manufacturer);
         $product['price'] = $this->currency->format($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
         if (isset($product['special']) && $product['special'] != null) {
